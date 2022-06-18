d31f233ff2b194b9dc460e9a04b6972d491f30ef
TRINITY003 <lawaloluwatobi404@gmail.com>
2022-06-18 02:32:28 -0700

    first commit

---
+++ TestApp/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+          System.Console.WriteLine("rahman!");
+      
+          
+          string name="This is a\"class\"";
+          System.Console.WriteLine(name);          
+      
+      
+      
+      
+        }
+    }
+}
+
+
+
+
---
+++ using System;.cs
@@ -0,0 +1,31 @@
+using System;  
+  
+namespace TestApp 
+  
+
+    class Program 
+     
+    {  
+        static void Main(string[] args)
+        {
+            static void Main(string[]args)
+            {
+                string name, input;
+                int age;
+                Console.WriteLine("What is your first name");
+                first name = Console.ReadLine();
+                Console.WriteLine("What is your last name");
+                last name = Console.ReadLine();
+                input = Console.ReadLine();
+                age = Convert.ToInt32(input);
+                Console.WriteLine("What is your favourite colour");
+                favorite colour = Console.ReadLine();
+                Console.WriteLine("What is your favorite food");
+                favorite food = Console.ReadLine();
+                Console.WriteLine("What is your state of origin");
+                state of origin = Console.ReadLine();
+            }
+
+        }  
+        
+    }
